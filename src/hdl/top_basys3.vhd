3f19a2198d0cdfb2b651e0fe87674f0462521191
26Reading <c26cian.reading@afacademy.af.edu>
2024-02-21 13:18:57 -0700

    top_basys3.vhd entity and architecture
    created sevenSegDecoder with vivado wizard

--- src/hdl/top_basys3.vhd
+++ src/hdl/top_basys3.vhd
@@ -79,20 +79,41 @@ end top_basys3;
 architecture top_basys3_arch of top_basys3 is 
 	
   -- declare the component of your top-level design unit under test (UUT)
-
-
+    component sevenSegDecoder is 
+        port (
+            i_D : in std_logic_vector(3 downto 0);
+            o_S : out std_logic_vector(6 downto 0)
+            --o_Sf : out std_logic;
+            --o_Se : out std_logic;
+           -- o_Sd : out std_logic;
+          --  o_Sc : out std_logic;
+            --o_Sb : out std_logic;
+            --o_Sa : out std_logic
+            --o_HexOut: out std_logic
+        );
+    end component sevenSegDecoder;
   -- create wire to connect button to 7SD enable (active-low)
-
+    signal w_7SD_EN_n : std_logic;
   
 begin
 	-- PORT MAPS ----------------------------------------
 
 	--	Port map: wire your component up to the switches and seven-segment display cathodes
 	-----------------------------------------------------	
+	segmentDisplay_inst: sevenSegDecoder
+	port map (
+	  i_D(3 downto 0)    => sw(3 downto 0),
+      o_S(6 downto 0)   => seg(6 downto 0)
 	
+	);
 	
 	-- CONCURRENT STATEMENTS ----------------------------
-	
+	w_7SD_EN_n  <= not btnC;
+	an(0)    <= w_7SD_EN_n;
+	-- turn all other anodes off (active low)
+    an(1)   <= '1';
+    an(2)   <= '1';
+    an(3)   <= '1';
 	-- wire up active-low 7SD anode (active low) to button (active-high)
 	-- display 7SD 0 only when button pushed
 	-- other 7SD are kept off
